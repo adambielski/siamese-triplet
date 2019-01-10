69c1073c634882867a374d38efed65b9d0b08a94
Adam Bielski <14027793+adambielski@users.noreply.github.com>
2019-01-10 15:49:26 +0100

    Added eps to contrastive loss sqrt

--- losses.py
+++ losses.py
@@ -12,11 +12,12 @@ class ContrastiveLoss(nn.Module):
     def __init__(self, margin):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
+        self.eps = 1e-9
 
     def forward(self, output1, output2, target, size_average=True):
         distances = (output2 - output1).pow(2).sum(1)  # squared distances
         losses = 0.5 * (target.float() * distances +
-                        (1 + -1 * target).float() * F.relu(self.margin - distances.sqrt()).pow(2))
+                        (1 + -1 * target).float() * F.relu(self.margin - (distances + self.eps).sqrt()).pow(2))
         return losses.mean() if size_average else losses.sum()
 
 
