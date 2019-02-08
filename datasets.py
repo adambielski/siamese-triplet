e4b738ef81f4c8987397443a43a1dd833b608193
Adam Bielski <14027793+adambielski@users.noreply.github.com>
2019-02-08 17:11:58 +0100

    BalancedBatchSampler is dataset independent now

--- datasets.py
+++ datasets.py
@@ -143,19 +143,14 @@ class TripletMNIST(Dataset):
         return len(self.mnist_dataset)
 
 
-
-
 class BalancedBatchSampler(BatchSampler):
     """
     BatchSampler - from a MNIST-like dataset, samples n_classes and within these classes samples n_samples.
     Returns batches of size n_classes * n_samples
     """
 
-    def __init__(self, dataset, n_classes, n_samples):
-        if dataset.train:
-            self.labels = dataset.train_labels
-        else:
-            self.labels = dataset.test_labels
+    def __init__(self, labels, n_classes, n_samples):
+        self.labels = labels
         self.labels_set = list(set(self.labels.numpy()))
         self.label_to_indices = {label: np.where(self.labels.numpy() == label)[0]
                                  for label in self.labels_set}
@@ -165,12 +160,12 @@ class BalancedBatchSampler(BatchSampler):
         self.count = 0
         self.n_classes = n_classes
         self.n_samples = n_samples
-        self.dataset = dataset
+        self.n_dataset = len(self.labels)
         self.batch_size = self.n_samples * self.n_classes
 
     def __iter__(self):
         self.count = 0
-        while self.count + self.batch_size < len(self.dataset):
+        while self.count + self.batch_size < self.n_dataset:
             classes = np.random.choice(self.labels_set, self.n_classes, replace=False)
             indices = []
             for class_ in classes:
@@ -185,4 +180,4 @@ class BalancedBatchSampler(BatchSampler):
             self.count += self.n_classes * self.n_samples
 
     def __len__(self):
-        return len(self.dataset) // self.batch_size
+        return self.n_dataset // self.batch_size
